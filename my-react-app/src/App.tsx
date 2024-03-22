0b01f4a2bf32c36a6fa14a8581d831d26ae7938c
CalebRdgz <calebrodgz@gmail.com>
2024-03-22 15:04:03 -0500

    Using props with Typescript

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -12,7 +12,11 @@ function App() {
       <Food />
       <Card />
       <Button />
-      <Student name="Spongebob" />
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student />
       <Footer />
     </>
   );
--- my-react-app/src/Student.tsx
+++ my-react-app/src/Student.tsx
@@ -1,13 +1,24 @@
 interface StudentProps {
-    name: string;
+  name?: string;
+  age?: number;
+  isStudent?: boolean;
 }
 
 const Student: React.FC<StudentProps> = (props) => {
+
   return (
-    <div>
+    <div className="student">
       <p>Name: {props.name}</p>
+      <p>Name: {props.age}</p>
+      <p>Student: {props.isStudent ? "Yes" : "No"}</p>
     </div>
   );
-}
+};
+
+Student.defaultProps = {
+  name: "Guest",
+  age: 20,
+  isStudent: false,
+};
 
 export default Student;
